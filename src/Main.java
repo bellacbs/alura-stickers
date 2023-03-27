a2288fd8e5a097dd74f053bbc25aada506384765
bellacbs <bellacbrandao@gmail.com>
2023-03-27 11:45:19 -0300

    top series

--- src/Main.java
+++ src/Main.java
@@ -8,22 +8,31 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args)  throws Exception{
 
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-
-        URI endereco = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        String body = response.body();
-
+        String urlFilmes = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        String urlSeries = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularTVs.json";
+        var req = new Request();
         var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
+        String bodyFilmes = req.get(urlFilmes);
+        List<Map<String, String>> listaDeFilmes = parser.parse(bodyFilmes);
         for (Map<String, String> filme: listaDeFilmes) {
             System.out.println(filme.get("title"));
             System.out.println(filme.get("image"));
             System.out.println(filme.get("imDbRating"));
             System.out.println();
         }
+
+        System.out.println("---------SERIES--------");
+
+        String bodySeries = req.get(urlSeries);
+        List<Map<String, String>> listaDeSeries = parser.parse(bodySeries);
+        for (Map<String, String> serie: listaDeSeries) {
+            System.out.println(serie.get("title"));
+            System.out.println(serie.get("image"));
+            System.out.println(serie.get("imDbRating"));
+
+        }
+
+
     }
 }
